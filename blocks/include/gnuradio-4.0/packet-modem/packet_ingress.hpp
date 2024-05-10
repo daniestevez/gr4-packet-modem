f076333774584f58b555efa44cf7fc92f731da4d
Daniel Estévez <daniel@destevez.net>
2024-05-10 12:13:22 +0200

    add packet ingress block
    Signed-off-by: Daniel Estévez <daniel@destevez.net>

---
+++ blocks/include/gnuradio-4.0/packet-modem/packet_ingress.hpp
@@ -0,0 +1,111 @@
+#ifndef _GR4_PACKET_MODEM_PACKET_INGRESS
+#define _GR4_PACKET_MODEM_PACKET_INGRESS
+
+#include <gnuradio-4.0/Block.hpp>
+#include <gnuradio-4.0/reflection.hpp>
+#include <ranges>
+
+namespace gr::packet_modem {
+
+template <typename T = uint8_t>
+class PacketIngress : public gr::Block<PacketIngress<T>>
+{
+public:
+    using Description = Doc<R""(
+@brief Packet Ingress. Accepts packets into a packet-based transmitter.
+
+This block is the point at which packets enter into packet-based
+transmitter. Packets are delimited at the input of this block by packet-length
+tags, and presented back-to-back. For each valid packet, the block sends a
+message to the header formatter indicating the length of the packet, so that the
+header formatter can produce a header to be attached to the packet. Invalid
+packets, which are those longer than the maximum packet length that fits in the
+header, are consumed and discarded, and a warning is printed.
+
+The block could be extended by the user to perform any other validation checks
+or payload preparation that is necessary.
+
+)"">;
+
+private:
+    const std::string d_packet_len_tag_key;
+    size_t d_remaining;
+    bool d_valid;
+
+public:
+    gr::PortIn<T> in;
+    gr::PortOut<T> out;
+    gr::MsgPortOut metadata;
+
+    constexpr static gr::TagPropagationPolicy tag_policy =
+        gr::TagPropagationPolicy::TPP_CUSTOM;
+
+    PacketIngress(const std::string& packet_len_tag_key = "packet_len")
+        : d_packet_len_tag_key(packet_len_tag_key), d_remaining(0)
+    {
+    }
+
+    gr::work::Status processBulk(const gr::ConsumableSpan auto& inSpan,
+                                 gr::PublishableSpan auto& outSpan)
+    {
+#ifdef TRACE
+        fmt::print("PacketIngress::processBulk(inSpan.size() = {}, outSpan.size() = {}), "
+                   "d_remaining = {}, d_valid = {}\n",
+                   inSpan.size(),
+                   outSpan.size(),
+                   d_remaining,
+                   d_valid);
+#endif
+        if (d_remaining == 0) {
+            // Fetch a new packet_len tag
+            static constexpr auto not_found =
+                "[PacketIngress] expected packet-length tag not found\n";
+            if (!this->input_tags_present()) {
+                throw std::runtime_error(not_found);
+            }
+            auto tag = this->mergedInputTag();
+            if (!tag.map.contains(d_packet_len_tag_key)) {
+                throw std::runtime_error(not_found);
+            }
+            d_remaining = pmtv::cast<uint64_t>(tag.map[d_packet_len_tag_key]);
+            d_valid = d_remaining <= std::numeric_limits<uint16_t>::max();
+            if (d_valid) {
+                // Use gr::message::Command::Invalid, since none of the OpenCMW
+                // commands is appropriate for GNU Radio 3.10-style message passing.
+                gr::sendMessage<gr::message::Command::Invalid>(
+                    metadata, "", "", { { "packet_length", d_remaining } });
+                out.publishTag(tag.map, 0);
+            } else {
+                fmt::print("[PacketIngress] packet too long (length {}); dropping\n",
+                           d_remaining);
+            }
+        } else if (d_valid && this->input_tags_present()) {
+            out.publishTag(this->mergedInputTag().map, 0);
+        }
+
+        auto to_consume = std::min(d_remaining, inSpan.size());
+        if (d_valid) {
+            to_consume = std::min(to_consume, outSpan.size());
+            std::ranges::copy_n(
+                inSpan.begin(), static_cast<ssize_t>(to_consume), outSpan.begin());
+            outSpan.publish(to_consume);
+        } else {
+            outSpan.publish(0);
+        }
+        std::ignore = inSpan.consume(to_consume);
+        d_remaining -= to_consume;
+
+        if (to_consume == 0) {
+            return inSpan.size() == 0 ? gr::work::Status::INSUFFICIENT_INPUT_ITEMS
+                                      : gr::work::Status::INSUFFICIENT_OUTPUT_ITEMS;
+        }
+
+        return gr::work::Status::OK;
+    }
+};
+
+} // namespace gr::packet_modem
+
+ENABLE_REFLECTION_FOR_TEMPLATE(gr::packet_modem::PacketIngress, in, out);
+
+#endif // _GR4_PACKET_PACKET_INGRESS
---
+++ examples/packet_ingress.cpp
@@ -0,0 +1,70 @@
+#include <gnuradio-4.0/Graph.hpp>
+#include <gnuradio-4.0/Scheduler.hpp>
+#include <gnuradio-4.0/packet-modem/message_debug.hpp>
+#include <gnuradio-4.0/packet-modem/packet_ingress.hpp>
+#include <gnuradio-4.0/packet-modem/packet_strobe.hpp>
+#include <gnuradio-4.0/packet-modem/vector_sink.hpp>
+#include <gnuradio-4.0/packet-modem/vector_source.hpp>
+#include <boost/ut.hpp>
+#include <thread>
+
+int main()
+{
+    using namespace boost::ut;
+
+    gr::Graph fg;
+
+    const std::vector<size_t> packet_lengths = { 10,     23, 2,     1024, 7,
+                                                 100000, 45, 51234, 28,   100 };
+    size_t offset = 0;
+    std::vector<gr::Tag> tags;
+    for (const auto len : packet_lengths) {
+        gr::property_map props = { { "packet_len", len } };
+        if (len > std::numeric_limits<uint16_t>::max()) {
+            props["invalid"] = true;
+        }
+        tags.push_back({ static_cast<ssize_t>(offset), props });
+        if (len > 50) {
+            tags.push_back(
+                { static_cast<ssize_t>(offset + 40), { { "packet_rest", true } } });
+        }
+        offset += len;
+    }
+    std::vector<uint8_t> v(offset);
+    auto& packet_source =
+        fg.emplaceBlock<gr::packet_modem::VectorSource<uint8_t>>(v, false, tags);
+
+    auto& ingress = fg.emplaceBlock<gr::packet_modem::PacketIngress<>>();
+    auto& sink = fg.emplaceBlock<gr::packet_modem::VectorSink<uint8_t>>();
+    auto& msg_debug = fg.emplaceBlock<gr::packet_modem::MessageDebug>();
+    expect(eq(gr::ConnectionResult::SUCCESS, ingress.metadata.connect(msg_debug.print)));
+    expect(eq(gr::ConnectionResult::SUCCESS,
+              fg.connect<"out">(packet_source).to<"in">(ingress)));
+    expect(eq(gr::ConnectionResult::SUCCESS, fg.connect<"out">(ingress).to<"in">(sink)));
+
+    gr::scheduler::Simple sched{ std::move(fg) };
+    gr::MsgPortOut toScheduler;
+    expect(eq(gr::ConnectionResult::SUCCESS, toScheduler.connect(sched.msgIn)));
+
+    std::thread stopper([&toScheduler]() {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::print("sending REQUEST_STOP to scheduler\n");
+        gr::sendMessage<gr::message::Command::Set>(toScheduler,
+                                                   "",
+                                                   gr::block::property::kLifeCycleState,
+                                                   { { "state", "REQUESTED_STOP" } });
+    });
+
+    expect(sched.runAndWait().has_value());
+    stopper.join();
+
+    const auto data = sink.data();
+    std::print("vector sink contains {} items\n", data.size());
+    std::print("vector sink tags:\n");
+    const auto sink_tags = sink.tags();
+    for (const auto& t : sink_tags) {
+        fmt::print("index = {}, map = {}\n", t.index, t.map);
+    }
+
+    return 0;
+}
