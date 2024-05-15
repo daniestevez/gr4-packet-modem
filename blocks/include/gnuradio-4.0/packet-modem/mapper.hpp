2d545e6c5f7de786ad06db6cc5723837e3f4774c
Daniel Estévez <daniel@destevez.net>
2024-05-15 12:29:51 +0200

    add mapper block
    Signed-off-by: Daniel Estévez <daniel@destevez.net>

---
+++ blocks/include/gnuradio-4.0/packet-modem/mapper.hpp
@@ -0,0 +1,54 @@
+#ifndef _GR4_PACKET_MODEM_MAPPER
+#define _GR4_PACKET_MODEM_MAPPER
+
+#include <gnuradio-4.0/Block.hpp>
+#include <gnuradio-4.0/reflection.hpp>
+#include <vector>
+
+namespace gr::packet_modem {
+
+template <typename TIn, typename TOut>
+class Mapper : public gr::Block<Mapper<TIn, TOut>>
+{
+public:
+    using Description = Doc<R""(
+@brief Mapper. Maps input elements according to a map defined by a vector.
+
+This block is constructed using a vector `map` whose size must be a power of 2,
+say `2^k`. For each input item, the block generates an output item by looking at
+the k LSBs of the input item and indexing the `map` using these k LSBs. The
+result is the output item. The data in the bits above the k LSBs in the input
+items is ignored.
+
+The block can be used for instance to implement a constellation modulator by
+mapping nibbles into constellation symbols.
+
+)"">;
+
+private:
+    const std::vector<TOut> d_map;
+    const size_t d_mask;
+
+public:
+    gr::PortIn<TIn> in;
+    gr::PortOut<TOut> out;
+
+    Mapper(const std::vector<TOut>& map) : d_map(map), d_mask(map.size() - 1)
+    {
+        if (!std::has_single_bit(map.size())) {
+            throw std::invalid_argument(std::format(
+                "[Mapper] the map size must be a power of 2 (got {})", map.size()));
+        }
+    }
+
+    [[nodiscard]] constexpr auto processOne(TIn a) const noexcept
+    {
+        return d_map[static_cast<size_t>(a) & d_mask];
+    }
+};
+
+} // namespace gr::packet_modem
+
+ENABLE_REFLECTION_FOR_TEMPLATE(gr::packet_modem::Mapper, in, out);
+
+#endif // _GR4_PACKET_MODEM_MAPPER
--- src/scratch.cpp
+++ src/scratch.cpp
@@ -1,58 +1,51 @@
-#include <fmt/core.h>
 #include <gnuradio-4.0/Graph.hpp>
 #include <gnuradio-4.0/Scheduler.hpp>
-#include <gnuradio-4.0/packet-modem/crc.hpp>
-#include <gnuradio-4.0/packet-modem/crc_append.hpp>
-#include <gnuradio-4.0/packet-modem/crc_check.hpp>
+#include <gnuradio-4.0/packet-modem/file_sink.hpp>
+#include <gnuradio-4.0/packet-modem/firdes.hpp>
 #include <gnuradio-4.0/packet-modem/interpolating_fir_filter.hpp>
+#include <gnuradio-4.0/packet-modem/mapper.hpp>
 #include <gnuradio-4.0/packet-modem/random_source.hpp>
-#include <gnuradio-4.0/packet-modem/vector_sink.hpp>
-#include <gnuradio-4.0/packet-modem/vector_source.hpp>
-#include <pmtv/pmt.hpp>
+#include <gnuradio-4.0/packet-modem/unpack_bits.hpp>
 #include <boost/ut.hpp>
-#include <numeric>
+#include <cmath>
+#include <complex>
 
-int main()
+int main(int argc, char* argv[])
 {
     using namespace boost::ut;
+    using c64 = std::complex<float>;
+
+    expect(fatal(argc == 2));
 
     gr::Graph fg;
     auto& source = fg.emplaceBlock<gr::packet_modem::RandomSource<uint8_t>>(
-        uint8_t{ 0 }, uint8_t{ 255 }, 1000U, false);
-    auto& sink = fg.emplaceBlock<gr::packet_modem::VectorSink<uint8_t>>();
-    expect(eq(gr::ConnectionResult::SUCCESS, fg.connect<"out">(source).to<"in">(sink)));
+        uint8_t{ 0 }, uint8_t{ 255 }, 1000000U);
+    auto& unpack = fg.emplaceBlock<gr::packet_modem::UnpackBits<>>(4U, uint8_t{ 2U });
+    const float a = std::sqrt(2.0f) / 2.0f;
+    const std::vector<c64> qpsk_constellation = {
+        { a, a }, { a, -a }, { -a, a }, { -a, -a }
+    };
+    auto& constellation_modulator =
+        fg.emplaceBlock<gr::packet_modem::Mapper<uint8_t, c64>>(qpsk_constellation);
+    const size_t sps = 4;
+    const size_t ntaps = sps * 11;
+    const auto rrc_taps = gr::packet_modem::firdes::root_raised_cosine(
+        1.0, static_cast<double>(sps), 1.0, 0.35, ntaps);
+    auto& rrc_interp =
+        fg.emplaceBlock<gr::packet_modem::InterpolatingFirFilter<c64, c64, float>>(
+            sps, rrc_taps);
+    auto& sink = fg.emplaceBlock<gr::packet_modem::FileSink<c64>>(argv[1]);
+    expect(eq(gr::ConnectionResult::SUCCESS, fg.connect<"out">(source).to<"in">(unpack)));
+    expect(eq(gr::ConnectionResult::SUCCESS,
+              fg.connect<"out">(unpack).to<"in">(constellation_modulator)));
+    expect(eq(gr::ConnectionResult::SUCCESS,
+              fg.connect<"out">(constellation_modulator).to<"in">(rrc_interp)));
+    expect(
+        eq(gr::ConnectionResult::SUCCESS, fg.connect<"out">(rrc_interp).to<"in">(sink)));
 
-    gr::scheduler::Simple sched{ std::move(fg) };
-
-    // gr::MsgPortOut toScheduler;
-    // expect(eq(gr::ConnectionResult::SUCCESS, toScheduler.connect(sched.msgIn)));
-    // std::thread stopper([&toScheduler]() {
-    //     std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    //     gr::sendMessage<gr::message::Command::Set>(toScheduler,
-    //                                                "",
-    //                                                gr::block::property::kLifeCycleState,
-    //                                                { { "state", "REQUESTED_STOP" } });
-    // });
-
-    const auto ret = sched.runAndWait();
-    // stopper.join();
-    if (!ret.has_value()) {
-        std::print("ret = {}\n", ret.error());
-    }
-    expect(ret.has_value());
-
-    const auto data = sink.data();
-    std::print("vector sink contains {} items\n", data.size());
-    std::print("vector sink items:\n");
-    for (const auto x : data) {
-        std::print("{} ", x);
-    }
-    std::print("\n");
-    std::print("vector sink tags:\n");
-    const auto sink_tags = sink.tags();
-    for (const auto& t : sink_tags) {
-        fmt::print("index = {}, map = {}\n", t.index, t.map);
-    }
+    gr::scheduler::Simple<gr::scheduler::ExecutionPolicy::multiThreaded> sched{ std::move(
+        fg) };
+    expect(sched.runAndWait().has_value());
 
     return 0;
 }
