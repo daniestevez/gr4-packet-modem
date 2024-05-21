f5deefd5db639cca3bdefa2315bac51a03a93005
Daniel Estévez <daniel@destevez.net>
2024-05-21 12:04:42 +0200

    add packet mux block
    The tagged-stream packet mux isn't working, because the runtime merges
    the input tags from different ports. There is a PDU specialization of
    the block that is working correctly.
    
    Signed-off-by: Daniel Estévez <daniel@destevez.net>

---
+++ blocks/include/gnuradio-4.0/packet-modem/packet_mux.hpp
@@ -0,0 +1,166 @@
+#ifndef _GR4_PACKET_MODEM_PACKET_MUX
+#define _GR4_PACKET_MODEM_PACKET_MUX
+
+#include <gnuradio-4.0/Block.hpp>
+#include <gnuradio-4.0/packet-modem/pdu.hpp>
+#include <gnuradio-4.0/reflection.hpp>
+#include <ranges>
+#include <vector>
+
+namespace gr::packet_modem {
+
+template <typename T>
+class PacketMux : public gr::Block<PacketMux<T>>
+{
+public:
+    using Description = Doc<R""(
+@brief Packet Mux. Concatenates one packet from each input to form an output packet.
+
+This block receives a stream of packets delimited with packet-length tags in
+each input port. It concatenatates a packet from each input in order to form an
+output packet, whose length is the sum of the lengths of these packets. The
+block waits until there is input available in each input port in order to
+produce a new output packet, since the packet-length tags on each input port are
+required to calculate the output packet-length tag.
+
+)"">;
+
+private:
+    const std::string d_packet_len_tag;
+    std::vector<size_t> d_packet_lens;
+    size_t d_current_input;
+    size_t d_remaining;
+
+public:
+    std::vector<gr::PortIn<T>> in;
+    gr::PortOut<T> out;
+
+    constexpr static TagPropagationPolicy tag_policy = TagPropagationPolicy::TPP_CUSTOM;
+
+    PacketMux(size_t num_inputs, const std::string& packet_len_tag = "packet_len")
+        : d_packet_len_tag(packet_len_tag), in(num_inputs)
+    {
+        if (num_inputs == 0) {
+            throw std::invalid_argument("[PacketMux] num_inputs cannot be zero");
+        }
+    }
+
+    template <gr::ConsumableSpan TInput>
+    gr::work::Status processBulk(const std::span<TInput>& inSpans,
+                                 gr::PublishableSpan auto& outSpan)
+    {
+#ifdef TRACE
+        fmt::print("PacketMux::processBulk(outSpan.size() = {}) ", outSpan.size());
+        for (size_t j = 0; j < inSpans.size(); ++j) {
+            fmt::print("inSpans[{}].size() = {} ", j, inSpans[j].size());
+        }
+        fmt::print("\n");
+#endif
+
+        if (d_packet_lens.empty()) {
+            // try to fetch packet lengths tags for each input
+            const auto min_in_size =
+                std::ranges::min(inSpans | std::views::transform([](const auto& span) {
+                                     return span.size();
+                                 }));
+            if (min_in_size == 0) {
+                return gr::work::Status::INSUFFICIENT_INPUT_ITEMS;
+            }
+            // TODO: this isn't working, because the tags from all the input
+            // ports get merged an overwritten
+            fmt::print("PacketMux::input_tags_present() = {}\n",
+                       this->input_tags_present());
+            if (this->input_tags_present()) {
+                const auto tag = this->mergedInputTag();
+                fmt::print(
+                    "PacketMux::mergedInputTag() = ({}, {})\n", tag.index, tag.map);
+            }
+            for (auto& inPort : in) {
+                fmt::print("getting tags for input port\n");
+                for (const auto& tag : inPort.getTags(min_in_size)) {
+                    fmt::print("tag = ({}, {})\n", tag.index, tag.map);
+                }
+            }
+        }
+
+        return gr::work::Status::DONE;
+    }
+};
+
+template <typename T>
+class PacketMux<Pdu<T>> : public gr::Block<PacketMux<Pdu<T>>>
+{
+public:
+    using Description = Doc<R""(
+@brief Packet Mux (PDU specialization). Concatenates one packet from each input
+to form an output packet.
+
+This block is a PDU specialization of the Packet Mux block. It receives a PDU on
+each input port and concatenates them to produce a PDU in the output port. The
+block waits until there is input available in each input port in order to
+produce a new output PDU.
+
+)"">;
+
+public:
+    std::vector<gr::PortIn<Pdu<T>>> in;
+    gr::PortOut<Pdu<T>> out;
+
+    PacketMux(size_t num_inputs) : in(num_inputs)
+    {
+        if (num_inputs == 0) {
+            throw std::invalid_argument("[PacketMux] num_inputs cannot be zero");
+        }
+    }
+
+    template <gr::ConsumableSpan TInput>
+    gr::work::Status processBulk(const std::span<TInput>& inSpans,
+                                 gr::PublishableSpan auto& outSpan)
+    {
+#ifdef TRACE
+        fmt::print("PacketMux<PDU<T>>::processBulk(outSpan.size() = {}) ",
+                   outSpan.size());
+        for (size_t j = 0; j < inSpans.size(); ++j) {
+            fmt::print("inSpans[{}].size() = {} ", j, inSpans[j].size());
+        }
+        fmt::print("\n");
+#endif
+
+        const auto min_in_size =
+            std::ranges::min(inSpans | std::views::transform(
+                                           [](const auto& span) { return span.size(); }));
+        const auto n = std::min(min_in_size, outSpan.size());
+
+        for (size_t j = 0; j < n; ++j) {
+            outSpan[j].data = inSpans[0][j].data;
+            outSpan[j].tags = inSpans[0][j].tags;
+            for (size_t k = 1; k < inSpans.size(); ++k) {
+                const ssize_t tag_offset = std::ssize(outSpan[j].data);
+                outSpan[j].data.insert(outSpan[j].data.end(),
+                                       inSpans[k][j].data.cbegin(),
+                                       inSpans[k][j].data.cend());
+                for (const auto& tag : inSpans[k][j].tags) {
+                    outSpan[j].tags.emplace_back(tag_offset + tag.index, tag.map);
+                }
+            }
+        }
+
+        for (const auto& span : inSpans) {
+            std::ignore = span.consume(n);
+        }
+        outSpan.publish(n);
+
+        if (min_in_size == 0) {
+            return gr::work::Status::INSUFFICIENT_INPUT_ITEMS;
+        }
+
+        return n == 0 ? gr::work::Status::INSUFFICIENT_OUTPUT_ITEMS
+                      : gr::work::Status::DONE;
+    }
+};
+
+} // namespace gr::packet_modem
+
+ENABLE_REFLECTION_FOR_TEMPLATE(gr::packet_modem::PacketMux, in, out);
+
+#endif // _GR4_PACKET_MODEM_PACKET_MUX
---
+++ test/qa_packet_mux.cpp
@@ -0,0 +1,51 @@
+#include <gnuradio-4.0/Graph.hpp>
+#include <gnuradio-4.0/Scheduler.hpp>
+#include <gnuradio-4.0/packet-modem/packet_mux.hpp>
+#include <gnuradio-4.0/packet-modem/pdu.hpp>
+#include <gnuradio-4.0/packet-modem/vector_sink.hpp>
+#include <gnuradio-4.0/packet-modem/vector_source.hpp>
+#include <boost/ut.hpp>
+#include <ranges>
+
+boost::ut::suite PacketMuxTests = [] {
+    using namespace boost::ut;
+    using namespace gr;
+    using namespace gr::packet_modem;
+
+    "pdu_packet_mux"_test = [] {
+        Graph fg;
+        const Pdu<int> pdu0 = { std::vector{ 1, 2, 3, 4, 5 },
+                                { { 0, { { "a", "b" } } }, { 3, { { "c", "d" } } } } };
+        const Pdu<int> pdu1 = { std::vector{ 6, 7, 8, 9 }, { { 2, { { "e", "f" } } } } };
+        const Pdu<int> pdu2 = { std::vector{ 10, 11, 12, 13, 14, 15 },
+                                { { 3, { { "h", "i" } } }, { 5, { { "j", "k" } } } } };
+        auto& source0 = fg.emplaceBlock<VectorSource<Pdu<int>>>(std::vector{ pdu0 });
+        auto& source1 = fg.emplaceBlock<VectorSource<Pdu<int>>>(std::vector{ pdu1 });
+        auto& source2 = fg.emplaceBlock<VectorSource<Pdu<int>>>(std::vector{ pdu2 });
+        auto& mux = fg.emplaceBlock<PacketMux<Pdu<int>>>(3U);
+        auto& sink = fg.emplaceBlock<VectorSink<Pdu<int>>>();
+        expect(eq(ConnectionResult::SUCCESS,
+                  fg.connect(source0, { "out" }, mux, { "in", 0 })));
+        expect(eq(ConnectionResult::SUCCESS,
+                  fg.connect(source1, { "out" }, mux, { "in", 1 })));
+        expect(eq(ConnectionResult::SUCCESS,
+                  fg.connect(source2, { "out" }, mux, { "in", 2 })));
+        expect(eq(ConnectionResult::SUCCESS, fg.connect<"out">(mux).to<"in">(sink)));
+        scheduler::Simple sched{ std::move(fg) };
+        expect(sched.runAndWait().has_value());
+        const auto data = sink.data();
+        expect(eq(data.size(), 1_u));
+        const auto out_pdu = data.at(0);
+        const std::vector<int> expected_items = { 1, 2,  3,  4,  5,  6,  7, 8,
+                                                  9, 10, 11, 12, 13, 14, 15 };
+        const std::vector<Tag> expected_tags = { { 0, { { "a", "b" } } },
+                                                 { 3, { { "c", "d" } } },
+                                                 { 7, { { "e", "f" } } },
+                                                 { 12, { { "h", "i" } } },
+                                                 { 14, { { "j", "k" } } } };
+        expect(eq(out_pdu.data, expected_items));
+        expect(out_pdu.tags == expected_tags);
+    };
+};
+
+int main() {}
