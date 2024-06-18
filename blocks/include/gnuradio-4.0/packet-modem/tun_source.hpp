8c0c387682eae5cedf9d03dacdb5c196a205ea07
Daniel Estévez <daniel@destevez.net>
2024-06-18 10:14:17 +0200

    add select() to TUN source
    Add select() call to avoid potentially waiting forever in the TUN
    source.
    
    Signed-off-by: Daniel Estévez <daniel@destevez.net>

--- blocks/include/gnuradio-4.0/packet-modem/tun_source.hpp
+++ blocks/include/gnuradio-4.0/packet-modem/tun_source.hpp
@@ -4,6 +4,7 @@
 #include <gnuradio-4.0/Block.hpp>
 #include <gnuradio-4.0/packet-modem/tun.hpp>
 #include <gnuradio-4.0/reflection.hpp>
+#include <sys/select.h>
 #include <array>
 #include <cerrno>
 #include <ranges>
@@ -23,6 +24,7 @@ public:
 
 public:
     gr::PortOut<Pdu<uint8_t>, gr::RequiredSamples<1U, 1U>> out;
+    double timeout = 1.0;
 
     void start() { open_tun(); }
 
@@ -33,7 +35,24 @@ public:
 #ifdef TRACE
         fmt::println("{}::processBulk(outSpan.size() = {})", this->name, outSpan.size());
 #endif
-        ssize_t ret = read(_tun_fd, _buff.data(), sizeof(_buff));
+        fd_set rfds;
+        struct timeval _timeout;
+        FD_ZERO(&rfds);
+        FD_SET(_tun_fd, &rfds);
+        _timeout.tv_sec = static_cast<int>(timeout);
+        _timeout.tv_usec = static_cast<uint32_t>(
+            std::round((timeout - static_cast<double>(_timeout.tv_sec)) * 1e6));
+        ssize_t ret = select(_tun_fd + 1, &rfds, nullptr, nullptr, &_timeout);
+        if (ret < 0) {
+            throw gr::exception(fmt::format("select() failed: {}", strerror(errno)));
+        } else if (ret == 0) {
+            // timeout expired; return to the scheduler
+            outSpan.publish(0);
+            return gr::work::Status::OK;
+        }
+
+        // select() says that the TUN has data; read it
+        ret = read(_tun_fd, _buff.data(), sizeof(_buff));
         if (ret < 0) {
             throw gr::exception(fmt::format("read failed: {}", strerror(errno)));
         }
@@ -49,6 +68,6 @@ public:
 
 } // namespace gr::packet_modem
 
-ENABLE_REFLECTION(gr::packet_modem::TunSource, out, tun_name, netns_name);
+ENABLE_REFLECTION(gr::packet_modem::TunSource, out, tun_name, netns_name, timeout);
 
 #endif // _GR4_PACKET_MODEM_TUN_SOURCE
