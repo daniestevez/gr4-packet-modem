48a8866e21801395b506742260ec182fa22c5d67
Daniel Estévez <daniel@destevez.net>
2024-09-05 09:58:57 +0200

    add more parameters to packet_mux_benchmark
    Signed-off-by: Daniel Estévez <daniel@destevez.net>

--- examples/packet_mux_benchmark.cpp
+++ examples/packet_mux_benchmark.cpp
@@ -20,12 +20,13 @@ int main(int argc, char* argv[])
     using namespace std::string_literals;
     using namespace gr::packet_modem;
 
-    if (argc != 5) {
+    if (argc != 7) {
         fmt::println(stderr,
                      "usage: {} mux_num_inputs packet_length "
-                     "tagged_to_pdu_output_buffer_size mux_in_max_samples",
+                     "tagged_to_pdu_output_buffer_size mux_in_max_samples"
+                     "mux_output_buffer_size pdu_to_stream_in_max_samples",
                      argv[0]);
-        fmt::println(stderr, "example usage: {} 2 6000 0 0", argv[0]);
+        fmt::println(stderr, "example usage: {} 2 6000 0 0 0 0", argv[0]);
         std::exit(1);
     }
 
@@ -37,6 +38,10 @@ int main(int argc, char* argv[])
     const size_t tagged_to_pdu_output_buffer_size = std::stoul(argv[3]);
     // 0UZ means do not set max_samples
     const size_t mux_in_max_samples = std::stoul(argv[4]);
+    // 0UZ means do not resizeBuffer()
+    const size_t mux_output_buffer_size = std::stoul(argv[5]);
+    // 0UZ means do not set max_samples
+    const size_t pdu_to_stream_in_max_samples = std::stoul(argv[6]);
 
     gr::Graph fg;
 
@@ -45,6 +50,10 @@ int main(int argc, char* argv[])
         for (auto& port : mux.in) {
             port.max_samples = mux_in_max_samples;
         }
+    }
+    if (mux_output_buffer_size != 0UZ) {
+        expect(fatal(eq(gr::ConnectionResult::SUCCESS,
+                        mux.out.resizeBuffer(mux_output_buffer_size))));
     }
 
     for (const auto j : std::views::iota(0UZ, mux_num_inputs)) {
@@ -65,6 +74,9 @@ int main(int argc, char* argv[])
     }
 
     auto& pdu_to_stream = fg.emplaceBlock<PduToTaggedStream<T>>();
+    if (pdu_to_stream_in_max_samples != 0UZ) {
+        pdu_to_stream.in.max_samples = pdu_to_stream.in.max_samples;
+    }
     auto& sink = fg.emplaceBlock<NullSink<T>>();
     auto& probe_rate = fg.emplaceBlock<ProbeRate<T>>();
     auto& message_debug = fg.emplaceBlock<MessageDebug>();
