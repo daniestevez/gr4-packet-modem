3b035c35c66e0546643b348fd1b4bdc044b3377a
Daniel Estévez <daniel@destevez.net>
2024-09-16 08:51:09 +0200

    add packet_transmitter benchmark flowgraph
    Signed-off-by: Daniel Estévez <daniel@destevez.net>

--- examples/benchmark_packet_transmitter_pdu.cpp
+++ examples/benchmark_packet_transmitter_pdu.cpp
@@ -3,7 +3,7 @@
 #include <gnuradio-4.0/packet-modem/message_debug.hpp>
 #include <gnuradio-4.0/packet-modem/null_sink.hpp>
 #include <gnuradio-4.0/packet-modem/packet_to_stream.hpp>
-#include <gnuradio-4.0/packet-modem/packet_transmitter.hpp>
+#include <gnuradio-4.0/packet-modem/packet_transmitter_pdu.hpp>
 #include <gnuradio-4.0/packet-modem/probe_rate.hpp>
 #include <gnuradio-4.0/packet-modem/vector_source.hpp>
 #include <complex>
@@ -25,29 +25,36 @@ int main(int argc, char** argv)
 
     gr::Graph fg;
     const std::vector<uint8_t> packet(packet_size);
-    const std::vector<gr::Tag> tags = { { 0, { { "packet_len", packet_size } } } };
+    const gr::packet_modem::Pdu<uint8_t> pdu = { packet, {} };
     auto& source =
-        fg.emplaceBlock<gr::packet_modem::VectorSource<uint8_t>>({ { "repeat", true } });
-    source.data = packet;
-    source.tags = tags;
-    auto packet_transmitter =
-        gr::packet_modem::PacketTransmitter(fg, stream_mode, samples_per_symbol);
+        fg.emplaceBlock<gr::packet_modem::VectorSource<gr::packet_modem::Pdu<uint8_t>>>(
+            { { "repeat", true } });
+    source.data = std::vector<gr::packet_modem::Pdu<uint8_t>>{ pdu };
+    const size_t max_in_samples = 1U;
+    // note that buffer size is rounded up to a multiple of
+    // lcm(sizeof(Pdu<T>), getpagesize()), but different values that round up to
+    // the same number give slightly different performance
+    const size_t out_buff_size = 1U;
+    auto packet_transmitter_pdu = gr::packet_modem::PacketTransmitterPdu(
+        fg, stream_mode, samples_per_symbol, max_in_samples, out_buff_size);
     auto& probe_rate = fg.emplaceBlock<gr::packet_modem::ProbeRate<c64>>();
     auto& message_debug = fg.emplaceBlock<gr::packet_modem::MessageDebug>();
 
     const char* connection_error = "connection_error";
 
-    if (fg.connect<"out">(source).to<"in">(*packet_transmitter.ingress) !=
-        gr::ConnectionResult::SUCCESS) {
-        throw gr::exception(connection_error);
-    }
-    if (!stream_mode) {
-        auto& packet_to_stream = fg.emplaceBlock<gr::packet_modem::PacketToStream<c64>>();
+    if (stream_mode) {
+        if (fg.connect<"out">(*packet_transmitter_pdu.rrc_interp).to<"in">(probe_rate) !=
+            gr::ConnectionResult::SUCCESS) {
+            throw gr::exception(connection_error);
+        }
+    } else {
+        auto& packet_to_stream = fg.emplaceBlock<
+            gr::packet_modem::PacketToStream<gr::packet_modem::Pdu<c64>>>();
         // Limit the number of samples that packet_to_stream can produce in one
         // call. Otherwise it produces 65536 items on the first call, and then "it
         // gets behind the Throttle block" by these many samples.
         packet_to_stream.out.max_samples = 1000U;
-        if (fg.connect<"out">(*packet_transmitter.burst_shaper)
+        if (fg.connect<"out">(*packet_transmitter_pdu.burst_shaper)
                 .to<"in">(packet_to_stream) != gr::ConnectionResult::SUCCESS) {
             throw gr::exception(connection_error);
         }
@@ -60,11 +67,11 @@ int main(int argc, char** argv)
             gr::ConnectionResult::SUCCESS) {
             throw gr::exception(connection_error);
         }
-    } else {
-        if (fg.connect<"out">(*packet_transmitter.rrc_interp_mult_tag)
-                .to<"in">(probe_rate) != gr::ConnectionResult::SUCCESS) {
-            throw gr::exception(connection_error);
-        }
+    }
+
+    if (fg.connect<"out">(source).to<"in">(*packet_transmitter_pdu.ingress) !=
+        gr::ConnectionResult::SUCCESS) {
+        throw gr::exception(connection_error);
     }
     if (fg.connect<"rate">(probe_rate).to<"print">(message_debug) !=
         gr::ConnectionResult::SUCCESS) {
