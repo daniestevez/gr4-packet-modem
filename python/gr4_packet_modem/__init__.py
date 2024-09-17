14d83e7a58b9573832b91eba32b7d87da8c3065b
Daniel Estévez <daniel@destevez.net>
2024-09-17 12:28:52 +0200

    start writing Python bindings for Graph
    Signed-off-by: Daniel Estévez <daniel@destevez.net>

--- python/gr4_packet_modem/__init__.py
+++ python/gr4_packet_modem/__init__.py
@@ -1,5 +1,1 @@
-from __future__ import annotations
-
-from .gr4_packet_modem_python import __doc__, __version__, add, subtract
-
-__all__ = ["__doc__", "__version__", "add", "subtract"]
+from .gr4_packet_modem_python import *
--- python/main.cpp
+++ python/main.cpp
@@ -1,14 +1,20 @@
+#include <pybind11/complex.h>
+#include <pybind11/numpy.h>
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+#include <gnuradio-4.0/Graph.hpp>
+#include <gnuradio-4.0/PluginLoader.hpp>
 
 #define STRINGIFY(x) #x
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
-
-int add(int i, int j) { return i + j; }
 
 namespace py = pybind11;
 
 PYBIND11_MODULE(gr4_packet_modem_python, m)
 {
+    py::module_::import("pmtv");
+
     m.doc() = R"pbdoc(
         gr4-packet-modem Python bindings proof of concept
         -------------------------------------------------
@@ -20,12 +26,23 @@ PYBIND11_MODULE(gr4_packet_modem_python, m)
 
     )pbdoc";
 
-    // Example function. To be removed
-    m.def("add", &add, R"pbdoc(
-        Add two numbers
+    py::class_<gr::PluginLoader>(m, "PluginLoader")
+        .def(py::init<gr::BlockRegistry&, std::span<const std::filesystem::path>>());
 
-        Some other explanation about the add function.
-    )pbdoc");
+    // m.def("globalPluginLoader", &gr::globalPluginLoader);
+
+    py::class_<gr::Graph>(m, "Graph")
+        .def(py::init<gr::property_map>(), py::arg("settings") = gr::property_map())
+        .def("emplaceBlock",
+             static_cast<gr::BlockModel& (gr::Graph::*)(std::string_view,
+                                                        std::string_view,
+                                                        gr::property_map,
+                                                        gr::PluginLoader&)>(
+                 &gr::Graph::emplaceBlock),
+             py::arg("type"),
+             py::arg("parameters"),
+             py::arg("initialSettings"),
+             py::arg("loader") /* = gr::globalPluginLoader()*/);
 
 #ifdef VERSION_INFO
     m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
